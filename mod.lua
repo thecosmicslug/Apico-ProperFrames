07825a810542b8b82660d35ff5967c5593b5671b
The Cosmic Slug <thecosmicslug@gmail.com>
2024-06-22 15:58:28 +0100

    Update mod.lua

--- mod.lua
+++ mod.lua
@@ -1,4 +1,4 @@
-MOD_NAME = "beemo"
+MOD_NAME = "ProperFrames"
 
 GLOBAL_RECIPE = {{ item = "log", amount = 1}, {item="sticks1", amount = 1}}
 
@@ -6,41 +6,29 @@ function register()
   return {
     name = MOD_NAME,
     hooks = {},
-    modules = {"minimizer", "maximizer", "enabler"}
+    modules = {}
   }
 end
 
 function init()
-  api_log("init", "BeeMO loaded")
-  api_log("init", "Beginning defines")
-  
-  api_log("init", "Defining Frame")
+  api_log("init", "ProperFrames loaded!")
+  api_log("init", "Defining Frame...")
   define_frame()
-
-  api_log("init", "Defining Minimizer")
-  define_minimizer()
-
-  api_log("init", "Defining Maximizer")
-  define_maximizer()
-
-  api_log("init", "Defining Enabler")
-  define_enabler()
-  
   return "Success"
 end
 
 function define_frame()
   api_define_item({
-    id = "unbreaking_frame",
-    name = "Unbreaking Frame",
+    id = "proper_frame",
+    name = "Proper Frame",
     category = "Beekeeping",
-    tooltip = "This Frame is Unbreakable!",
+    tooltip = "This Frame is decent!",
     shop_key = false,
-    shop_buy = 0,
+    shop_buy = 10,
     shop_sell = 0,
-    durability = 10000,
+    durability = 50,
     singular = true,
     machines = {"hive1", "hive2"}
   },"sprites/unbreaking_frame.png")
-  res = api_define_recipe("beekeeping", MOD_NAME .. "_unbreaking_frame", GLOBAL_RECIPE, 1)
+  res = api_define_recipe("beekeeping", MOD_NAME .. "_proper_frame", GLOBAL_RECIPE, 1)
 end
